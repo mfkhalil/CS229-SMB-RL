d622206c8436ed10a6cdc1fb0a693753db6555b1
EC2 Default User <ec2-user@ip-172-31-15-209.us-west-1.compute.internal>
2023-05-12 08:17:10 +0000

    update

--- train.py
+++ train.py
@@ -3,6 +3,6 @@ from stable_baselines3 import PPO
 
 
 env = utils.make_env()
-callback = utils.CallbackFunction(models_path=utils.MODEL_DIR, videos_path=utils.VIDEO_DIR, save_frequency=50000)
+callback = utils.CallbackFunction(models_path=utils.MODEL_DIR, videos_path=utils.VIDEO_DIR, save_frequency=500)
 model = PPO('CnnPolicy', env, verbose=1, tensorboard_log=utils.LOG_DIR, learning_rate=0.000001, n_steps=512)
 model.learn(total_timesteps=1000000, callback=callback)
