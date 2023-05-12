41cc187bc5861e2f75cd70811e47fcdb8dfb55dd
EC2 Default User <ec2-user@ip-172-31-15-209.us-west-1.compute.internal>
2023-05-12 08:58:53 +0000

    update steps per callback

--- train.py
+++ train.py
@@ -3,6 +3,6 @@ from stable_baselines3 import PPO
 
 
 env = utils.make_env()
-callback = utils.CallbackFunction(models_path=utils.MODEL_DIR, save_frequency=500)
+callback = utils.CallbackFunction(models_path=utils.MODEL_DIR, save_frequency=10000)
 model = PPO('CnnPolicy', env, verbose=1, tensorboard_log=utils.LOG_DIR, learning_rate=0.000001, n_steps=512)
 model.learn(total_timesteps=1000000, callback=callback)
